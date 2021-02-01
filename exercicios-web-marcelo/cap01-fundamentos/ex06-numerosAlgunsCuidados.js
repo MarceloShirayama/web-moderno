7f6800bc491a0141f132055c8607c7d6fae8e617
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-01 16:56:07 -0300

    concluido ex06

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex06-numerosAlgunsCuidados.js
@@ -0,0 +1,12 @@
+console.log((7 / 0));  // = infinity
+
+console.log('10' / 5);  // = 2
+console.log('10.2' / 5);  // = 2
+console.log('10,2' / 5);  // = NaN
+
+console.log('Show' * 2);  // = NaN
+
+console.log(0.1 + 0.7);  // = 0.799999.... => problema de precisão no cálculo de ponto flutuante
+
+// console.log(10.toString());  => não pode
+console.log((10).toString());  // => pode
