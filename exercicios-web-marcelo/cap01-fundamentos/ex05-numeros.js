5325d9e8fbb9aabd59e96095b93652386c996dbb
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-01 16:45:36 -0300

    concluido ex05

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex05-numeros.js
@@ -0,0 +1,21 @@
+const peso1 = 1.0;
+console.log(peso1);
+console.log(typeof peso1);
+console.log(Number.isInteger(peso1));
+
+const peso2 = Number('2.3');
+console.log(peso2);
+console.log(typeof peso2);
+console.log(Number.isInteger(peso2));
+
+const  avaliacao1 = 9.871;
+const avaliacao2 = 6.852;
+
+const media = (avaliacao1 * peso1 + avaliacao2 * peso2)  / (peso1 + peso2);
+console.log(media.toFixed(1));
+
+console.log(media.toString());
+console.log(media.toString(2));  // valor binario de média
+console.log(media.toFixed(1).toString());
+
+console.log(typeof media);
