fc77e5e7e0afad2546d9ca08dbd445474f40768c
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-01 17:50:51 -0300

    concluido ex08 e alterado ex06

--- exercicios-web-marcelo/cap01-fundamentos/ex06-numerosAlgunsCuidados.js
+++ exercicios-web-marcelo/cap01-fundamentos/ex06-numerosAlgunsCuidados.js
@@ -10,3 +10,6 @@ console.log(0.1 + 0.7);  // = 0.799999.... => problema de precisão no cálculo 
 
 // console.log(10.toString());  => não pode
 console.log((10).toString());  // => pode
+
+console.log('3' + 2);  // = 32 => nesse caso serão concatenados os valores
+console.log('3' - 2);  // = 1
