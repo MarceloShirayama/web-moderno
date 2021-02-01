7a5ed9e2e9df7e2627436c68804e7d94cffd6e0a
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-01 16:02:55 -0300

    concluido ex03

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex03-VarLetConst.js
@@ -0,0 +1,24 @@
+// var pode ser  reatribuído o valor e pode ser redeclarada a variável
+var a = 3;
+// let  pode ser  reatribuído o valor e não pode ser redeclarada a variável
+let b = 4;
+// const  não pode ser  reatribuído o valor e não pode ser redeclarada a variável
+const c = 5;
+
+a = 30;
+
+var a = 300;
+
+b = 40;
+
+// let b = 40; => não pode
+
+// c = 50; => não pode
+
+// const c = 50; => não pode
+
+console.log(a, b, c);
+
+// Utilizar sempre let ou const para declarar variáveis.
+
+// mas preferir const e só usar let se realmente precisar redefinir o valor da variável.
