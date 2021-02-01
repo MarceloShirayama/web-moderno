b6c7ff8a2635767fcff1fc8d11dbe88e5e233d0b
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-01 19:17:34 -0300

    concluido ex09

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex09-templateString.js
@@ -0,0 +1,19 @@
+const nome = 'Rebeca';
+
+const concatenacao = `Olá ${nome}!`;
+
+console.log(nome);
+console.log(concatenacao);
+
+const html = `
+  Olá
+  ${nome}!
+` ;
+
+console.log(html);
+
+console.log(`1 + 1 = ${1 + 1}`);
+
+const  up = texto => texto.toUpperCase();
+
+console.log(`Ei... ${up('cuidado')}!`);
