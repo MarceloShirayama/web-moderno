63e6b891dddd27775e6c49c76a23f0fd7da15700
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-01 16:28:57 -0300

    concluido ex04

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex04-tipagemFraca.js
@@ -0,0 +1,13 @@
+let a = 2;
+
+console.log(`a === ${a}`);
+console.log(typeof a);
+
+a = 'dois';
+
+console.log(`a === ${a}`);
+console.log(typeof a);
+
+console.log('Em JS a tipagem é fraca.');
+console.log('Na reatribuição de uma variável podemos alterar seu tipo');
+console.log('Como no caso de number para string');
