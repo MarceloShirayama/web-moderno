0d361c7484ffe7118ed807103ad4dd4c112863f6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-02 17:39:03 -0300

    concluido ex21

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex21usandoLetEmLoop1.js
@@ -0,0 +1,5 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+// console.log('i = ', i); => ReferenceError: i is not defined
+// variável definida dentro do bloco com let não é visível fora do bloco
