0a89b3d0d3bfadae9a9cb33706ee6bb3419f3e7a
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-02 17:53:49 -0300

    concluido ex23

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex23-usandoLetELoop2.js
@@ -0,0 +1,9 @@
+const funcs = [];
+
+for (let i = 0; i < 10; i++) {
+  funcs.push(() => console.log(i));
+}
+
+funcs[2]();
+funcs[8]();
+// executando com let é guardado o valor de i quando foi executada funcs
