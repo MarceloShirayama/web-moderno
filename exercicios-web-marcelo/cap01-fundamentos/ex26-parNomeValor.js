ba74d0f8966486b3dca764068f837f45618161d3
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-02 20:51:45 -0300

    concluido ex26

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex26-parNomeValor.js
@@ -0,0 +1,9 @@
+const saudacao = 'Opa'; // contexto lexico 1
+
+function exec() {
+  const saudacao = 'Falaaa'; // contexto léxico 2
+  return saudacao;
+}
+
+console.log(saudacao);
+console.log(exec());
