a8ddd8af689fb794efd9d5b640f9c1766bc35916
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-02 14:57:36 -0300

    concluido ex16

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex16-funcao2.js
@@ -0,0 +1,12 @@
+// Armazenado uma função em uma variável
+const imprimirSoma = function (a, b) {
+  console.log(a + b);
+};
+imprimirSoma(2, 3);
+
+// Armazenado uma função arrow em uma variável
+const sum = (a, b) => console.log(a + b);
+sum(20, 30);
+
+const cumprimento = saudacao => console.log(`${saudacao}!!!`);
+cumprimento('Olá');
