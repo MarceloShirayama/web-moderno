365b5a229257e848f8ddb6e0681c04e799712eb9
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-02 20:41:46 -0300

    concluido ex25

---
+++ exercicios-web-marcelo/cap01-fundamentos/ex25-object2.js
@@ -0,0 +1,10 @@
+console.log(`typeof Object: ${typeof Object}`);
+console.log(`typeof new Object(): ${typeof new Object()}`);
+
+const Cliente = function () {};
+console.log(`typeof const Cliente = function () {}: ${typeof Cliente}`);
+
+console.log(`typeof new Cliente(): ${typeof new Cliente()}`);
+
+class Produto {}
+console.log(`typeof class Produto {}: ${typeof Produto}`);
