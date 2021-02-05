0384b056102247aaf190c27c6777fb92cdb5ed02
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-04 22:30:21 -0300

    done ex06

---
+++ exercicios-web-marcelo/cap03-funcao/ex06-thisEBind.js
@@ -0,0 +1,14 @@
+/* eslint-disable new-parens */
+/* eslint-disable no-new */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+function Pessoa() {
+  this.idade = 0;
+  const self = this;
+  setInterval(function () {
+    self.idade++;
+    console.log(self.idade);
+  }/* .bind(this) */, 1000);
+}
+
+new Pessoa;
