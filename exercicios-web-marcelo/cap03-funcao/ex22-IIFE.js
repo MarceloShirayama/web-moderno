a8b1f3b82399a9adffc3a80b52f0ba0eba2a3008
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-05 20:59:22 -0300

    done ex22

---
+++ exercicios-web-marcelo/cap03-funcao/ex22-IIFE.js
@@ -0,0 +1,9 @@
+// IIFE -> Immediately Invoked Function Expression
+
+// eslint-disable-next-line func-names, wrap-iife
+(function () {
+  console.log('Será executado na hora');
+  console.log('Foge do escopo global');
+})();
+
+// FIXME: wrap-iife
