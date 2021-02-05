a34314b4405871265e5d46936bf7197c847225d2
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-05 11:42:29 -0300

    done ex13

---
+++ exercicios-web-marcelo/cap03-funcao/ex13-callback3.js
@@ -0,0 +1,5 @@
+// exemplo de callback para excutar no browser
+
+document.getElementsByTagName('body')[0]
+// eslint-disable-next-line no-unused-vars
+  .onclick = (e) => console.log('O evento ocorreu.');
