819aaf59d282502123ec56d470d43393f88b440b
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-06 11:57:14 -0300

    done ex03

---
+++ exercicios-web-marcelo/cap03-funcao/Exercicios/ex03.js
@@ -0,0 +1,6 @@
+// Crie uma função que recebe dois parâmetros, base e expoente,
+// e retorne a base elevada ao expoente.
+
+const expoente = (base, exp) => base ** exp;
+
+console.log(expoente(2, 3));
