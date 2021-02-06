23fafa9b82999a69f9dedcca3b7c2ab24fc6fccc
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-06 07:52:39 -0300

    solved fixme

--- exercicios-web-marcelo/cap03-funcao/ex22-IIFE.js
+++ exercicios-web-marcelo/cap03-funcao/ex22-IIFE.js
@@ -6,4 +6,10 @@
   console.log('Foge do escopo global');
 })();
 
-// FIXME: wrap-iife
+// DONE: wrap-iife
+/*
+Existe também uma sintaxe diferente para a IIFE. Podemos mover o segundo par de
+parênteses para dentro do primeiro par, envolvendo assim a expressão inteira
+com o primeiro par de parênteses.
+O resultado é idêntico, tratando-se apenas de uma decisão de estilo.
+*/
