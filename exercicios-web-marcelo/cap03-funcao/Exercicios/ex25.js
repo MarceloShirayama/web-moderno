27af30f35cc280500d8341c514098979f46174df
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-09 13:33:42 -0300

    done ex25

---
+++ exercicios-web-marcelo/cap03-funcao/Exercicios/ex25.js
@@ -0,0 +1,17 @@
+/*
+25) Escrever um programa para exibir os números de 1 até 50 na tela.
+*/
+
+let i = 1;
+while (i <= 50) {
+  console.log(i);
+  // eslint-disable-next-line no-plusplus
+  i++;
+}
+
+console.log('===================');
+console.log('ou');
+
+for (let index = 1; index <= 50; index++) {
+  console.log(index);
+}
