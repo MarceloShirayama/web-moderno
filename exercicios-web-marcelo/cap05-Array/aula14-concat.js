818a0f7893c57273ed6ee02a3c8e295ec3f473a2
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-15 13:30:27 -0300

    done aula14

---
+++ exercicios-web-marcelo/cap05-Array/aula14-concat.js
@@ -0,0 +1,10 @@
+const filhas = ['Ualeskah', 'Cibalena'];
+const filhos = ['Uoxiton', 'Uesclei'];
+
+const todos = filhas.concat(filhos);
+console.log(todos);
+console.log(filhas, filhos);
+
+console.log();
+console.log([].concat([1, 2], [3, 4], [5, 6]));
+console.log([].concat([1, 2], [3, 4], [5, 6], 7, 8, 9, 10));
