b7f26981b3a2ad548c4854a0392f9a23e4ac934f
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2021-02-15 15:06:29 -0300

    redone ex01

--- exercicios-web-marcelo/cap05-Array/Exercicios/ex01.js
+++ exercicios-web-marcelo/cap05-Array/Exercicios/ex01.js
@@ -1,5 +1,15 @@
-function cumprimentar(nome) {
+function cumprimentar1(nome) {
   return 'Olá, '.concat(nome, '!');
 }
+console.log(cumprimentar1('Leonardo'));
 
-console.log(cumprimentar('Leonardo'));
+function cumprimentar2(nome) {
+  return `Olá, ${nome}!`;
+}
+console.log(cumprimentar2('Maira'));
+
+function cumprimentar3(nome) {
+  const saudacao = 'Olá';
+  return [saudacao, nome].join(', ').concat('!');
+}
+console.log(cumprimentar3('João'));
