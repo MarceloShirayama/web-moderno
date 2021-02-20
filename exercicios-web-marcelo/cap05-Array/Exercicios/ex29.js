be8569e1e273e72dc7c97552d4b2743e7a7491dc
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-02-19 21:37:16 -0300

    done ex29

---
+++ exercicios-web-marcelo/cap05-Array/Exercicios/ex29.js
@@ -0,0 +1,36 @@
+/*
+Crie uma função que recebe um array de números e retorna o segundo maior número presente
+nesse array.
+Exemplos:
+segundoMaior([12, 16, 1, 5]) // retornará 12
+segundoMaior([8, 4, 5, 6]) // retornará 6
+*/
+
+console.log('Resolução 1:');
+
+const numbers1 = [12, 16, 1, 5];
+const numbers2 = [8, 4, 5, 6];
+
+function searchSecondLargest1(arrayOfNumbers) {
+  const firstLargest = Math.max(...arrayOfNumbers);
+  const arrayClone = [...arrayOfNumbers]; // clone para o splice não alterar o array original
+  arrayClone.splice(arrayClone.indexOf(firstLargest), 1);
+  const secondLargest = Math.max(...arrayClone);
+  return secondLargest;
+}
+
+console.log(searchSecondLargest1(numbers1));
+console.log(searchSecondLargest1(numbers2));
+
+console.log('=================');
+
+console.log('Resolução 2:');
+
+function secondLargest2(arrayOfNumbers) {
+  const sortDecrescent = (a, b) => b - a;
+  const sorted = (array) => array.sort(sortDecrescent);
+  return sorted(arrayOfNumbers)[1];
+}
+
+console.log(secondLargest2(numbers1));
+console.log(secondLargest2(numbers2));
