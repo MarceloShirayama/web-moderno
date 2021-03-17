80a65b4f28e83c816a9456ab3da08c39f7f29e38
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-03-17 16:32:35 -0300

    implementing the server.js

---
+++ exercicios-web-marcelo/cap12-Ajax/server.js
@@ -0,0 +1,10 @@
+import bodyParser from 'body-parser';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(8080, () => console.log('run in localhost:8080'));
