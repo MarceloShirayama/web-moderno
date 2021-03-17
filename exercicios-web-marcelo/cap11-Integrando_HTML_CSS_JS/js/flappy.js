6de6082ab6f6fcdbf5cc633c3cb3e3de83fb2101
MarceloShirayama <caixadecorreiodomarcelo@gmail>
2021-03-17 10:59:33 -0300

    done flappy03

--- exercicios-web-marcelo/cap11-Integrando_HTML_CSS_JS/js/flappy.js
+++ exercicios-web-marcelo/cap11-Integrando_HTML_CSS_JS/js/flappy.js
@@ -50,5 +50,41 @@ class ParDeBarreiras {
   }
 }
 
-const b = new ParDeBarreiras(700, 200, 500);
-document.querySelector('[wm-flappy]').appendChild(b.elemento);
+// const b = new ParDeBarreiras(700, 200, 500);
+// document.querySelector('[wm-flappy]').appendChild(b.elemento);
+
+class Barreiras {
+  constructor(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+      new ParDeBarreiras(altura, abertura, largura),
+      new ParDeBarreiras(altura, abertura, largura + espaco),
+      new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+      new ParDeBarreiras(altura, abertura, largura + espaco * 3),
+    ];
+
+    const deslocamento = 3;
+    this.animar = () => {
+      this.pares.forEach((par) => {
+        par.setX(par.getX() - deslocamento);
+
+        // quando o elemento sair da tela
+        if (par.getX() < -par.getLargura()) {
+          par.setX(par.getX() + espaco * this.pares.length);
+          par.sortearAbertura();
+        }
+
+        const meio = largura / 2;
+        const cruzouOMeio = par.getX() + deslocamento >= meio
+          && par.getX() < meio;
+        if (cruzouOMeio) notificarPonto();
+      });
+    };
+  }
+}
+
+const barreiras = new Barreiras(700, 1100, 200, 400);
+const areaDoJogo = document.querySelector('[wm-flappy');
+barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.elemento));
+setInterval(() => {
+  barreiras.animar();
+}, 20);
